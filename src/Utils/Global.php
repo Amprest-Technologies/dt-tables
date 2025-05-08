9244a75078fc595a53b553d7584b6e0456179d91
Alvin Kaburu <geekaburu@amprest.co.ke>
2025-05-08 12:55:25 +0300

    Minimal updates and sanitizations

--- src/Utils/Global.php
+++ src/Utils/Global.php
@@ -1,13 +1,3 @@
 <?php
 
-if (! function_exists('package_path')) {
-    /**
-     * Generate a package resouce view name depending on the package.
-     *
-     * @author Alvin Gichira Kaburu <geekaburu@amprest.co.ke>
-     */
-    function package_path($path): mixed
-    {
-        return config('laravel-dt.name').'::'.$path;
-    }
-}
+//
